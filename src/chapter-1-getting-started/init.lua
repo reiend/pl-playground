a75080f85f13419226c6ab3fb5d2bf7ad49ccf65
reiend <loncerasjakerei@gmail.com>
2022-12-26 00:51:33 +0800

    some lexical coventions

--- src/chapter-1-getting-started/init.lua
+++ src/chapter-1-getting-started/init.lua
@@ -10,3 +10,24 @@ local firstname = 'jake'
 
 print('Hello my name is ' .. firstname .. ', I am ' .. age .. ' years old')
 print 'Nice to meet you'
+
+print('Half of my age is ' .. age / 2)
+
+-- run the program then enter ton interactive mode
+-- lua -i <program-path>
+
+-- will immediately execute the program from the interactive mode
+-- dofile(<program-path>)
+
+-- Lexical Conventions
+-- lua is case sensitive
+-- <--> is for single line comment
+-- <--[[> is for multi line comment
+-- <---[[> adding one on the opening tag will run the code
+---[[
+  print 'hello' -- this code will run
+  print 'hello'
+  print 'hello'
+  print 'hello'
+-- ]]
+
