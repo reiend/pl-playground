fa3e153a59bd635707c50565e3f09f4379f66999
reiend <loncerasjakerei@gmail.com>
2022-12-26 00:07:34 +0800

    ps script for running chapter 1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,17 @@
-lua .\init.lua
+$FILE_NAME = "init.lua"
+$CHAPTER_ONE = "chapter-1-getting-started\$($FILE_NAME)"
+
+function Invoke-Run
+{
+  Write-Host "Enter chapter number #: "
+  $ChapterNumber = Read-Host
+
+  if($ChapterNumber -eq 1)
+  {
+    Write-Host "Chapter 1 - Getting Started"
+    lua ".\src\$($CHAPTER_ONE)"
+  }
+}
+
+Invoke-Run
+
