64d4a67abbcf43047b9e9b1f82559ee51c56d713
reiend <loncerasjakerei@gmail.com>
2022-12-26 00:35:05 +0800

    greetings with lua

--- src/chapter-1-getting-started/init.lua
+++ src/chapter-1-getting-started/init.lua
@@ -1,2 +1,12 @@
 print 'hello world'
 
+-- Chunk
+-- each piece of lua code that get executed are called chunks
+-- sequence of commands
+
+print(math.pi)
+local age = 22
+local firstname = 'jake'
+
+print('Hello my name is ' .. firstname .. ', I am ' .. age .. ' years old')
+print 'Nice to meet you'
