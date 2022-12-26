6af6c0351bd2dbde773c6d5bdbe183d886857eb7
reiend <loncerasjakerei@gmail.com>
2022-12-26 12:54:09 +0800

    exercise 2.2

---
+++ src/chapter-1-getting-started/exercise-1-2/init.lua
@@ -0,0 +1,16 @@
+-- 1.2
+-- Run the twice example, both by loading the file with
+-- the -l option and with dofile. Which way do you prefer?
+-- ignore warnings needed to be global
+
+-- with -dofile
+-- lua
+-- dofile("lib.lua")
+
+-- with -l
+-- lua -llib .\init.lua
+-- I prefer -l prefiex
+
+n = norm(3.4, 1.0)
+print(n)
+print(twice(n))
---
+++ src/chapter-1-getting-started/exercise-1-2/lib.lua
@@ -0,0 +1,9 @@
+-- ignore warnings needed to be global
+function norm(x, y)
+  return math.sqrt(x ^ 2 + y ^ 2)
+end
+
+function twice(x)
+  return 2.0 * x
+end
+
